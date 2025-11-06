cf51bb4c3461a73fca63f3612207e6724e6e72a5
Ketan Salvi <ketan.salvi777@gmail.com>
2025-11-06 21:35:26 +0530

    str and rune

--- GOlang/youtube/Alex Mux/cmd/tutorial_5/main.go
+++ GOlang/youtube/Alex Mux/cmd/tutorial_5/main.go
@@ -7,10 +7,24 @@ import (
 func main() {
 	var myString string = "résumé"
 	fmt.Println(myString)
-	var idx = myString[3]
+	var idx = myString[1]
 	fmt.Printf("%v, %T\n", idx, idx)
-
 	for idx, val := range myString {
 		fmt.Println(idx, val)
 	}
+	fmt.Printf("The length of 'myString' is %v\n", len(myString))
+
+	var myRune = []rune("résumé")
+	fmt.Println(myRune)
+	var index = myRune[1]
+	fmt.Printf("%v, %T\n", index, index)
+	for index, val := range myRune {
+		fmt.Println(index, val)
+	}
+
+	fmt.Printf("The length of 'myRune' is %v\n", len(myRune))
+
+	var myRune2 = 'a'
+	fmt.Printf("myRune2 = %v", myRune2)
+
 }
