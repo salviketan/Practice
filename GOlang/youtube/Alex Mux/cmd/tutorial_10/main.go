57e1e425f52f180110dd0e427b58e33be779a1cb
Ketan Salvi <ketan.salvi777@gmail.com>
2025-11-15 12:14:46 +0530

    buggy code:- practicing syntax

--- GOlang/youtube/Alex Mux/cmd/tutorial_10/main.go
+++ GOlang/youtube/Alex Mux/cmd/tutorial_10/main.go
@@ -11,6 +11,15 @@ func main() {
 	fmt.Println(sumSlice(intSlice))
 	fmt.Println(sumSlice(float32Slice))
 	fmt.Println(sumSlice(float64Slice))
+
+	fmt.Println(sumSlice2(intSlice))
+
+	var a petrolEngine = petrolEngine{kmph: 5, gallon: 2}
+
+	fmt.Printf("%v", a.gallon)
+
+	engine[petrolEngine](a)
+
 }
 
 func sumSlice[T int | float32 | float64](slice []T) T {
@@ -19,4 +28,22 @@ func sumSlice[T int | float32 | float64](slice []T) T {
 		sum += v
 	}
 	return sum
+}
+
+func isEmpty[T any](slice []T) bool {
+	retrun len(slice) == 0
+}
+
+type petrolEngine struct {
+	kmph   int16
+	gallon int16
+}
+
+type diselEngine struct {
+	kmph   int16
+	gallon int16
+}
+
+func engine[e petrolEngine | diselEngine](a e) {
+	fmt.Println(a)
 }
