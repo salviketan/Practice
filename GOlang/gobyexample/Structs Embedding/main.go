d7bc4d912a0e669c901148400de8fead2d844b96
Ketan Salvi <ketan.salvi777@gmail.com>
2025-11-26 12:44:07 +0530

    Embedding self-ver

---
+++ GOlang/gobyexample/Structs Embedding/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type base struct {
+	num int
+}
+
+func (b base) discribe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	base
+	str string
+}
+
+func main() {
+	co := container{
+		base: base{
+			num: 1,
+		},
+		str: "some name",
+	}
+
+	fmt.Printf("co={num: %d, str: %s}\n", co.num, co.str)
+	fmt.Printf("also num: %v\n", co.base.num)
+
+	fmt.Println("discribe:", co.discribe())
+
+	var d discriber = co
+	fmt.Printf("describer: %s", d.discribe())
+}
+
+type discriber interface {
+	discribe() string
+}
