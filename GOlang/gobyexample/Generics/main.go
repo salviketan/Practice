390afb2884e033cc72e125cd87e0ce4f11e9fdcf
Ketan Salvi <ketan.salvi777@gmail.com>
2025-11-27 12:10:59 +0530

    Generics

--- GOlang/gobyexample/Generics/main.go
+++ GOlang/gobyexample/Generics/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
+func slicesIndex[S ~[]E, E comparable](s S, v E) int {
+	for i := range s {
+		if v == s[i] {
+			return i
+		}
+	}
+	return -1
+}
+
 type List[T any] struct {
 	head, tail *element[T]
 }
@@ -7,4 +20,38 @@ type List[T any] struct {
 type element[T any] struct {
 	next *element[T]
 	val  T
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.head == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) AllElements() []T {
+	var elem []T
+	for e := lst.head; e != nil; e = e.next {
+		elem = append(elem, e.val)
+	}
+	return elem
+}
+
+func main() {
+	var s = []string{"foo", "bar", "zoo"}
+
+	fmt.Println("index of zoo:", slicesIndex(s, "zoo"))
+
+	_ = slicesIndex[[]string, string](s, "zoo")
+
+	var lst = List[string]{}
+
+	lst.Push("10")
+	lst.Push("13")
+	lst.Push("23")
+	fmt.Println("list:", lst.AllElements())
+
 }
