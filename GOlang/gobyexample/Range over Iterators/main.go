13dbd43d3641ba5f13f9c831a238b5eed1771b34
Ketan Salvi <ketan.salvi777@gmail.com>
2025-11-30 23:59:13 +0530

    Range over Iterators part-2

--- GOlang/gobyexample/Range over Iterators/main.go
+++ GOlang/gobyexample/Range over Iterators/main.go
@@ -8,3 +8,12 @@ type element[T any] struct {
 	next *element[T]
 	val  T
 }
+
+func (lst *List[T]) AllElements() []T {
+	var elem []T
+
+	for e := lst.head; e != nil; e = e.next {
+		elem = append(elem, e.val)
+	}
+	return elem
+}
