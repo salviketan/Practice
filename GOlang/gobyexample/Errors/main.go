71849365b23ba750c17cb58af12c1c8d2ce9b8f5
Ketan Salvi <ketan.salvi777@gmail.com>
2025-12-03 00:06:14 +0530

    Errors

--- GOlang/gobyexample/Errors/main.go
+++ GOlang/gobyexample/Errors/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func f(arg int) (int, error) {
-	if arg == 2 {
+	if arg == 42 {
 		return -1, errors.New("can't work with 42")
 	}
 	return arg + 3, nil
@@ -36,12 +36,41 @@ func makeTea(arg int) error {
 
 func main() {
 	arg := 2
-
-	result := makeTea(arg)
-
-	if result != nil {
-		fmt.Println("error:", result)
+	is_error := makeTea(arg)
+	if is_error != nil {
+		fmt.Println("error:", is_error)
 	} else {
 		fmt.Println("arg: ", arg)
+	}
+
+	for _, i := range []int{7, 42} {
+		if r, e := f(i); e != nil {
+			fmt.Println("f failed:", e)
+		} else {
+			fmt.Println("f worked:", r)
+		}
+	}
+
+	for i := range 5 {
+		if e := makeTea(i); e != nil {
+			// switch e {
+			// case ErrOutOfTea:
+			// 	fmt.Println("We should buy new tea!")
+			// case ErrPower:
+			// 	fmt.Println("Now it is dark.")
+			// default:
+			// 	fmt.Println("i",i)
+			// }
+
+			if errors.Is(e, ErrOutOfTea) {
+				fmt.Println("We should buy new tea!")
+			} else if errors.Is(e, ErrPower) {
+				fmt.Println("Now it is dark.")
+			} else {
+				fmt.Printf("unknown error %s\n", e)
+			}
+			continue
+		}
+		fmt.Println("Tea is ready!")
 	}
 }
