e6b3a0c1e5af03aab8a227203ce8de3bec46462d
Ketan Salvi <ketan.salvi777@gmail.com>
2025-12-11 10:40:32 +0530

    Channel Synchronization - understd

--- GOlang/gobyexample/Channel Synchronization/main.go
+++ GOlang/gobyexample/Channel Synchronization/main.go
@@ -7,15 +7,22 @@ import (
 
 func worker(done chan bool) {
 	fmt.Print("Working...")
-	time.Sleep(time.Second)
+	time.Sleep(time.Duration(3) * time.Second)
 	fmt.Println("done")
 	done <- true
 }
 
 func main() {
+	t0 := time.Now()
 	done := make(chan bool, 1)
 
+	// worker(done)
 	go worker(done)
 
+	fmt.Println("before blocking receive")
+
 	<-done
+
+	fmt.Println("after blocking receive")
+	fmt.Println(time.Since(t0))
 }
