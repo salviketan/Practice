54bcbe7873ff9ddd13260048ce096326bb5c15d2
Ketan Salvi <ketan.salvi777@gmail.com>
2025-12-12 09:50:27 +0530

    Channel Directions

---
+++ GOlang/gobyexample/Channel Directions/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+
+func main() {
+	pings := make(chan string)
+	pongs := make(chan string)
+
+	go ping(pings, "passed msg")
+	go pong(pings, pongs)
+
+	fmt.Println(<-pongs)
+}
