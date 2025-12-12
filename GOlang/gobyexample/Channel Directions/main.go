49efcdfff40830c9efadb56ff24b982e82c8ad20
Ketan Salvi <ketan.salvi777@gmail.com>
2025-12-12 11:03:57 +0530

    Channel Directions - comment chng

--- GOlang/gobyexample/Channel Directions/main.go
+++ GOlang/gobyexample/Channel Directions/main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 // pi1 chan<- string :- Meaning of this line is we can send data(in this case "string") to the
-// passed channel in func arg
+// passed channel in func arg.
 func ping(pi1 chan<- string, msg string) {
 	pi1 <- msg
 	fmt.Println("pi1:", pi1)
 }
 
 // pings <-chan string :- Meaning of this line is we can recieve data(in this case "string") from
-// the passed channel in arg
+// the passed channel in arg.
 // pongs chan<- string :- Meaning of this line is we can send data(in this case "string") to the
-// passed channel in arg
+// passed channel in arg.
 func pong(pi2 <-chan string, po1 chan<- string) {
 	fmt.Println("pi2:", pi2)
 	fmt.Println("po1:", po1)
