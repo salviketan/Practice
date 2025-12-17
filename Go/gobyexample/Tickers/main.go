30797067edb2f93da4279480f5b973aa089e6d60
Ketan Salvi <ketan.salvi777@gmail.com>
2025-12-17 11:51:35 +0530

    Tickers - Goroutine Leak

--- Go/gobyexample/Tickers/main.go
+++ Go/gobyexample/Tickers/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
+	fmt.Println("Goroutines at start:", runtime.NumGoroutine())
 
 	ticker := time.NewTicker(500 * time.Millisecond)
 	done := make(chan bool)
@@ -23,7 +25,13 @@ func main() {
 
 	time.Sleep(1600 * time.Millisecond)
 	ticker.Stop()
-	done <- true
+	done <- true // comment this line to see Goroutine Leak/Zombie goroutines
 	fmt.Println("Ticker stopped")
 
+	fmt.Println("Goroutines at end:", runtime.NumGoroutine())
+
 }
+
+// NOTE:- runtime.NumGoroutine() - "Use this to print the number of active goroutines in our code.
+// We have used this to find out Goroutine Leaks in our code by printing number of active goroutines
+// before and after our logic."
