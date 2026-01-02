04b1b63e24328105e281228caf66fa1ed56b53a9
Ketan Salvi <ketan.salvi777@gmail.com>
2026-01-02 12:27:54 +0530

    String Functions

---
+++ Go/gobyexample/String Functions/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+func main() {
+	p := fmt.Println
+
+	p("Contains:  ", s.Contains("test", "es"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+	p("Replace:   ", s.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s.Replace("foo", "o", "0", 1))
+	p("Split:     ", s.Split("a-b-c-d", "-"))
+	p("ToLower:   ", s.ToLower("TEST"))
+	p("ToUpper:   ", s.ToUpper("test"))
+}
