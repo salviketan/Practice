6ebe875869cfa1c58153185882409a2197009cad
salviketan <ketan.salvi777@gmail.com>
2026-01-04 21:58:58 +0530

    String Formatting - Part2

--- Go/gobyexample/String Formatting/main.go
+++ Go/gobyexample/String Formatting/main.go
@@ -16,4 +16,28 @@ func main() {
 	fmt.Printf("struct3: %#v\n", p)
 
 	fmt.Printf("type: %T\n", p)
+
+	fmt.Printf("bool: %t\n", true)
+
+	fmt.Printf("int: %d\n", 123)
+
+	fmt.Printf("bin: %b\n", 14)
+
+	fmt.Printf("char: %c\n", 33)
+
+	fmt.Printf("hex: %x\n", 456)
+
+	fmt.Printf("float1: %f\n", 78.9)
+
+	fmt.Printf("float2: %e\n", 123400000.0)
+
+	fmt.Printf("float3: %E\n", 123400000.0)
+
+	fmt.Printf("str1: %s\n", "\"string\"")
+
+	fmt.Printf("str2: %q\n", "\"string\"")
+
+	fmt.Printf("str3: %x\n", "hex this")
+
+	fmt.Printf("pointer: %p\n", &p)
 }
