7e74f99789b8f45742dfeff3d272457014d6fc01
salviketan <ketan.salvi777@gmail.com>
2026-01-23 21:26:54 +0530

    go romanToInt_2 23-01-2026

--- DSA/LeetCode/Go/roman2Int.go
+++ DSA/LeetCode/Go/roman2Int.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
-func romanToInt(s string, v map[string]int) int {
+func romanToInt_1(s string) int {
+	value := map[rune]int{
+		'I': 1, 'V': 5, 'X': 10, 'L': 50,
+		'C': 100, 'D': 500, 'M': 1000,
+	}
+
 	total := 0
 	i := 0
 	for i < len(s) {
-		if (i+1) < len(s) && v[string(s[i])] < v[string(s[i+1])] {
-			total += v[string(s[i+1])] - v[string(s[i])]
+		if (i+1) < len(s) && value[rune(s[i])] < value[rune(s[i+1])] {
+			total += value[rune(s[i+1])] - value[rune(s[i])]
 			i += 2
 		} else {
-			total += v[string(s[i])]
+			total += value[rune(s[i])]
 			i += 1
 		}
 	}
@@ -18,13 +23,39 @@ func romanToInt(s string, v map[string]int) int {
 	return total
 }
 
-func main() {
-	inp_str := "MCMXCIV"
-	val := map[string]int{
+// Approach 2: Left-to-Right Pass Improvalueed
+func romanToInt_2(s string) int {
+	value := map[string]int{
 		"I": 1, "V": 5, "X": 10, "L": 50,
-		"C": 100, "D": 500, "M": 1000,
+		"C": 100, "D": 500, "M": 1000, "IV": 4,
+		"IX": 9, "XL": 40, "XC": 90, "CD": 400,
+		"CM": 900,
 	}
 
-	fmt.Println("romanToInt: ", romanToInt(inp_str, val))
+	total := 0
+	i := 0
+	for i < len(s) {
+		if i < (len(s)-1) && value[string(s[i])] < value[string(s[i+1])] {
+			total += value[string(s[i:i+2])]
+			i += 2
+		} else {
+			total += value[string(s[i])]
+			i += 1
+		}
+	}
 
+	return total
+}
+
+// Approach 3: Right-to-Left pass
+func romanToInt_3(s string) {
+
+}
+
+func main() {
+	inp_str := "MCMXCIV"
+
+	fmt.Println("romanToInt_1: ", romanToInt_1(inp_str))
+	fmt.Println("romanToInt_2: ", romanToInt_2(inp_str))
+	// fmt.Println("romanToInt_1: ", romanToInt_3(inp_str))
 }
