082147f47f116b5ee1d15a00d7b17425d3a3c34f
salviketan <ketan.salvi777@gmail.com>
2026-01-25 23:41:02 +0530

    go romanToInt_4 LC_impl

--- DSA/LeetCode/Go/roman2Int.go
+++ DSA/LeetCode/Go/roman2Int.go
@@ -48,6 +48,7 @@ func romanToInt_2(s string) int {
 }
 
 // Approach 3: Right-to-Left pass
+// My approach
 func romanToInt_3(s string) int {
 	values := map[rune]int{
 		'I': 1, 'V': 5, 'X': 10, 'L': 50,
@@ -70,10 +71,36 @@ func romanToInt_3(s string) int {
 	return total
 }
 
+// LeetCode approach
+func romanToInt_4(s string) int {
+
+	values := map[rune]int{
+		'I': 1, 'V': 5, 'X': 10, 'L': 50,
+		'C': 100, 'D': 500, 'M': 1000,
+	}
+
+	lastValue := values[rune(s[len(s)-1])]
+	total := lastValue
+
+	for i := len(s) - 2; i >= 0; i-- {
+		CurrentValue := values[rune(s[i])]
+
+		if lastValue > CurrentValue {
+			total -= CurrentValue
+		} else {
+			total += CurrentValue
+		}
+		lastValue = CurrentValue
+	}
+
+	return total
+}
+
 func main() {
 	inp_str := "MCMXCIV"
 
 	fmt.Println("romanToInt_1: ", romanToInt_1(inp_str))
 	fmt.Println("romanToInt_2: ", romanToInt_2(inp_str))
 	fmt.Println("romanToInt_3: ", romanToInt_3(inp_str))
+	fmt.Println("romanToInt_4: ", romanToInt_4(inp_str))
 }
