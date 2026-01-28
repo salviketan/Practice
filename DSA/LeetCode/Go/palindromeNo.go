8ba6c065a18cbfe0ff461d0b093a2f3bd98c5730
salviketan <ketan.salvi777@gmail.com>
2026-01-28 23:36:02 +0530

    go palindromeNo 28-01-2026

--- DSA/LeetCode/Go/palindromeNo.go
+++ DSA/LeetCode/Go/palindromeNo.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func isPalindrome() {
+func isPalindrome(x int) bool {
+	var reverse int
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	for x > reverse {
+		reverse = reverse*10 + (x % 10)
+		x /= 10
+	}
 
+	return reverse == x || reverse/10 == x
 }
 
 func main() {
 	fmt.Println("start")
+	fmt.Println(isPalindrome(121))
 }
