053c8e04415e99f3b505f6df549dad562163f76b
salviketan <ketan.salvi777@gmail.com>
2026-01-30 21:42:25 +0530

    go twoSum apprch3 30-01-2026

--- DSA/LeetCode/Go/twoSum.go
+++ DSA/LeetCode/Go/twoSum.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
+// Approach 3: One-pass Hash Table
 func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		complement := target - num
+		if j, ok := m[complement]; ok {
+			return []int{j, i}
+		}
+		m[num] = i
+	}
 
+	return []int{}
 }
 
 func main() {
-	fmt.Println("twoSum")
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 }
