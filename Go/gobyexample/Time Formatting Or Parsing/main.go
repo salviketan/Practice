7530f5059832b8cb5d6bb4001362a20bd4e96b4f
salviketan <ketan.salvi777@gmail.com>
2026-03-04 20:06:47 +0530

    go version updated code

--- Go/gobyexample/Time Formatting Or Parsing/main.go
+++ Go/gobyexample/Time Formatting Or Parsing/main.go
@@ -11,12 +11,12 @@ func main() {
 	t := time.Now()
 	p(t.Format(time.RFC3339))
 
-	t1, e := time.Parse(
+	t1, _ := time.Parse(
 		time.RFC3339,
 		"2012-11-01T22:08:41+00:00")
-	if e != nil {
-		p(e)
-	}
+	// if e != nil {
+	// 	p(e)
+	// }
 	p(t1)
 
 	p(t1.Format("03:04 PM"))
