870013f8cdf86e120efc31799e9eef284e0d7557
salviketan <ketan.salvi777@gmail.com>
2026-07-25 21:34:32 +0530

    URL Parsing - Part1

--- Go/gobyexample/62_URL Parsing/main.go
+++ Go/gobyexample/62_URL Parsing/main.go
@@ -6,9 +6,21 @@ import (
 )
 
 func main()  {
-	s, err := url.Parse("postgres://user:pass@host.com:5432/path?k=v#f")
+	l, err := url.Parse("postgres://user:pass@host.com:5432/path?k=v#f")
 	if err != nil{
 		panic(err)
 	}
-	fmt.Println(s)
+	fmt.Println(l)
+
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println("Scheme:",u.Scheme)
+
+    fmt.Println("User:",u.User)
+    fmt.Println(u.User.Username())
+
 }
