a7aeda748e9e9b3c08f4306a0a199efa16649efb
salviketan <ketan.salvi777@gmail.com>
2026-07-31 18:30:56 +0530

    Panic - +comment

--- Go/gobyexample/48_Panic/panic.go
+++ Go/gobyexample/48_Panic/panic.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	panic("A problem") //If you’d like to see the program try to create a temp file, comment this line
 
-	path := filepath.Join(os.TempDir(), "file")
+	path := filepath.Join(os.TempDir(), "file")	//file is created in "/tmp/" dir for safety and platform dependency
 	_, err := os.Create(path)
 	if err != nil {
 		panic(err)
