114a9091652bb4755e20383abaa0d739e50c5917
salviketan <ketan.salvi777@gmail.com>
2026-08-18 23:13:41 +0530

    89_HTTP Server - name chng

--- Go/gobyexample/89_HTTP Server/http-server.go
+++ Go/gobyexample/89_HTTP Server/http-server.go
@@ -11,10 +11,10 @@ func hello(w http.ResponseWriter, req *http.Request)  {
 	fmt.Fprintf(w, "hello\n")
 }
 
-func handler(w http.ResponseWriter, req *http.Request)  {
+func headers(w http.ResponseWriter, req *http.Request)  {
 	
-	for name, header := range req.Header {
-		for _, h := range header {
+	for name, headers := range req.Header {
+		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
 		}
 	}
@@ -24,7 +24,7 @@ func handler(w http.ResponseWriter, req *http.Request)  {
 func main()  {
 	
 	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/header", handler)
+	http.HandleFunc("/headers", headers)
 
 	http.ListenAndServe(":8090", nil)
 }
